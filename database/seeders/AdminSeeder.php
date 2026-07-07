d0d3f0d821ed651c6da0bd3f9c0c1b944743fa3d
brilliantnone-lang <brilliantnone@gmail.com>
2026-07-07 20:33:13 +0800

    fix

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -8,9 +8,6 @@ use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
         User::create([
